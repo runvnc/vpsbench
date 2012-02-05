a85a5c3a89eafbd94c10e8a79667cbbbacfb3099
Jason Livesay <ithkuil@gmail.com>
2012-02-04 20:02:50 -0800

    update

--- processor.coffee
+++ processor.coffee
@@ -15,34 +15,37 @@ exports.parse = (data) ->
   parts = []
   obj = {}
   for section in sections
-    lines = section.split "\n"
-    keep = (line for line in lines when line isnt '---')
-    name = keep[0]
-    switch name
-      when "Provider" then      val = keep[2]
-      when "RAM" then           val = keep[1]
-      when "date" then          val = new Date(keep[2])
-      when "Network"
-        val = justNum keep[2]
-        val = 100.0 / val
-      when "IP address"
-        val = keep[2].match(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)[0]
-        #val = justNum ips[0]
-      when "Disk Write", "Disk Read"
-        val = keep[4]
-        val = val.match /[0-9\.]+\ [a-zA-Z]+\/s/
-        val = val[0]
-        val = val.split ' '
-        val = val[0]
-      else                      val = justNum keep[2]
-    obj[name] = val
+    try 
+      lines = section.split "\n"
+      keep = (line for line in lines when line isnt '---')
+      name = keep[0]
+      switch name
+        when "Provider" then      val = keep[2]
+        when "RAM" then           val = keep[1]
+        when "date" then          val = new Date(keep[2])
+        when "Network"
+          val = justNum keep[2]
+          val = 100.0 / val
+        when "IP address"
+          val = keep[2].match(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)[0]
+          #val = justNum ips[0]
+        when "Disk Write", "Disk Read"
+          val = keep[4]
+          val = val.match /[0-9\.]+\ [a-zA-Z]+\/s/
+          val = val[0]
+          val = val.split ' '
+          val = val[0]
+        else                      val = justNum keep[2]
+      obj[name] = val
+    catch e
   console.log "returning from parse"
   console.log util.inspect(obj)
   obj
 
 exports.newdata = (data) ->
-  record = exports.parse data
-  results = db.collection 'results'
-  console.log "inserting "
-  results.insert record
-  
+  try
+    record = exports.parse data
+    results = db.collection 'results'
+    results.insert record
+  catch e
+    console.log e 
--- processor.js
+++ processor.js
@@ -24,46 +24,50 @@
     obj = {};
     for (_i = 0, _len = sections.length; _i < _len; _i++) {
       section = sections[_i];
-      lines = section.split("\n");
-      keep = (function() {
-        var _j, _len2, _results;
-        _results = [];
-        for (_j = 0, _len2 = lines.length; _j < _len2; _j++) {
-          line = lines[_j];
-          if (line !== '---') _results.push(line);
+      try {
+        lines = section.split("\n");
+        keep = (function() {
+          var _j, _len2, _results;
+          _results = [];
+          for (_j = 0, _len2 = lines.length; _j < _len2; _j++) {
+            line = lines[_j];
+            if (line !== '---') _results.push(line);
+          }
+          return _results;
+        })();
+        name = keep[0];
+        switch (name) {
+          case "Provider":
+            val = keep[2];
+            break;
+          case "RAM":
+            val = keep[1];
+            break;
+          case "date":
+            val = new Date(keep[2]);
+            break;
+          case "Network":
+            val = justNum(keep[2]);
+            val = 100.0 / val;
+            break;
+          case "IP address":
+            val = keep[2].match(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)[0];
+            break;
+          case "Disk Write":
+          case "Disk Read":
+            val = keep[4];
+            val = val.match(/[0-9\.]+\ [a-zA-Z]+\/s/);
+            val = val[0];
+            val = val.split(' ');
+            val = val[0];
+            break;
+          default:
+            val = justNum(keep[2]);
         }
-        return _results;
-      })();
-      name = keep[0];
-      switch (name) {
-        case "Provider":
-          val = keep[2];
-          break;
-        case "RAM":
-          val = keep[1];
-          break;
-        case "date":
-          val = new Date(keep[2]);
-          break;
-        case "Network":
-          val = justNum(keep[2]);
-          val = 100.0 / val;
-          break;
-        case "IP address":
-          val = keep[2].match(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)[0];
-          break;
-        case "Disk Write":
-        case "Disk Read":
-          val = keep[4];
-          val = val.match(/[0-9\.]+\ [a-zA-Z]+\/s/);
-          val = val[0];
-          val = val.split(' ');
-          val = val[0];
-          break;
-        default:
-          val = justNum(keep[2]);
+        obj[name] = val;
+      } catch (e) {
+
       }
-      obj[name] = val;
     }
     console.log("returning from parse");
     console.log(util.inspect(obj));
@@ -72,10 +76,13 @@
 
   exports.newdata = function(data) {
     var record, results;
-    record = exports.parse(data);
-    results = db.collection('results');
-    console.log("inserting ");
-    return results.insert(record);
+    try {
+      record = exports.parse(data);
+      results = db.collection('results');
+      return results.insert(record);
+    } catch (e) {
+      return console.log(e);
+    }
   };
 
 }).call(this);
